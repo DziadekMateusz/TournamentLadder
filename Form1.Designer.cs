76f7591e4860f2487fe8011e65dd05c187b6d2bf
Mateusz Dziadek <mateusz.dziadek03@gmail.com>
2026-03-15 21:42:09 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TournamentLadder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxRight = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLeft = new System.Windows.Forms.PictureBox();
+            this.buttonRefreshPorts = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxRight
+            // 
+            this.pictureBoxRight.BackColor = System.Drawing.Color.White;
+            this.pictureBoxRight.Location = new System.Drawing.Point(481, 127);
+            this.pictureBoxRight.Name = "pictureBoxRight";
+            this.pictureBoxRight.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRight.TabIndex = 17;
+            this.pictureBoxRight.TabStop = false;
+            // 
+            // pictureBoxLeft
+            // 
+            this.pictureBoxLeft.BackColor = System.Drawing.Color.White;
+            this.pictureBoxLeft.Location = new System.Drawing.Point(171, 127);
+            this.pictureBoxLeft.Name = "pictureBoxLeft";
+            this.pictureBoxLeft.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLeft.TabIndex = 16;
+            this.pictureBoxLeft.TabStop = false;
+            // 
+            // buttonRefreshPorts
+            // 
+            this.buttonRefreshPorts.Location = new System.Drawing.Point(126, 76);
+            this.buttonRefreshPorts.Name = "buttonRefreshPorts";
+            this.buttonRefreshPorts.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefreshPorts.TabIndex = 15;
+            this.buttonRefreshPorts.Text = "Refresh";
+            this.buttonRefreshPorts.UseVisualStyleBackColor = true;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(168, 40);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(52, 16);
+            this.labelStatus.TabIndex = 14;
+            this.labelStatus.Text = "Waiting";
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(29, 35);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxPorts.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(29, 76);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 9;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxRight);
+            this.Controls.Add(this.pictureBoxLeft);
+            this.Controls.Add(this.buttonRefreshPorts);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Tournament Ladder";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxRight;
+        private System.Windows.Forms.PictureBox pictureBoxLeft;
+        private System.Windows.Forms.Button buttonRefreshPorts;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonConnect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TournamentLadder
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort = new SerialPort();   // Serial port (USB/COM)
+        List<string> imageFiles = new List<string>(); // List of photo locations
+        Random rnd = new Random();                    // Random number generator
+        string currentLeft = "";                      // The path of the current photo on the left
+        string currentRight = "";                     // The path of the current photo on the right
+
+        public Form1()
+        {
+            InitializeComponent();
+            RefreshPorts();                                    // Search for available COM ports
+            button1.Enabled = button2.Enabled = button3.Enabled = false; // Disable buttons until a connection is established
+            serialPort.DataReceived += SerialPort_DataReceived; // Handling the data reception event from Arduino
+        }
+
+        private void RefreshPorts()
+        {
+            comboBoxPorts.Items.Clear();                       // Clear the port list
+            comboBoxPorts.Items.AddRange(SerialPort.GetPortNames()); // Retrieving available COM ports
+
+            if (comboBoxPorts.Items.Count > 0)
+                comboBoxPorts.SelectedIndex = 0;               // Automatic selection of the first port
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort.IsOpen)                       // Check if the port is already open
+                {
+                    serialPort.PortName = comboBoxPorts.SelectedItem.ToString(); // Selected port
+                    serialPort.BaudRate = 9600;               // Setting the baud rate (Arduino-compatible)
+                    serialPort.Open();                        // Port Opening
+                    labelStatus.Text = "Connected to " + serialPort.PortName;
+                    button1.Enabled = button2.Enabled = button3.Enabled = true; // Enabling the send buttons
+                    LoadImages();                             // Loading photos
+                    ShowRandomImages();                       // Random display of two images
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection error: " + ex.Message);
+            }
+        }
+
+        private void LoadImages()
+        {
+            string folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Pictures"); // Folder location
+
+            if (!Directory.Exists(folder))
+            {
+                MessageBox.Show("The 'Pictures' folder does not exist in the project directory!");
+                return;
+            }
+
+            // Download JPG/PNG/JPEG files
+            imageFiles = Directory.GetFiles(folder, "*.*")
+                                  .Where(f => f.EndsWith(".jpg") || f.EndsWith(".png") || f.EndsWith(".jpeg"))
+                                  .ToList();
+
+            if (imageFiles.Count < 2)
+            {
+                MessageBox.Show("Not enough photos in the Pictures folder (min. 2)");
+                return;
+            }
+        }
+
+        private void ShowRandomImages()
+        {
+            if (imageFiles.Count < 2) return;
+
+            var list = new List<string>(imageFiles); // A copy of the list for the drawing
+
+            currentLeft = list[rnd.Next(list.Count)]; // Drawing for the photo on the left
+            list.Remove(currentLeft);                 // Removing a photo from the pool
+
+            currentRight = list[rnd.Next(list.Count)]; // Drawing for the photo on the right
+
+            pictureBoxLeft.ImageLocation = currentLeft;   // Display the photo on the left
+            pictureBoxRight.ImageLocation = currentRight; // Display the photo on the right
+        }
+
+        private void ReplaceRightImage()
+        {
+            imageFiles.Remove(currentRight);         // Removing the photo on the right from the pool
+            if (imageFiles.Count == 0) LoadImages(); // If you've run out of photos, reload the page
+
+            string newImg;
+            do newImg = imageFiles[rnd.Next(imageFiles.Count)]; // Draw new photo
+            while (newImg == currentLeft);                      // Preventing duplication on the left
+
+            currentRight = newImg;
+            pictureBoxRight.ImageLocation = newImg;             // Display a new photo
+        }
+
+        private void ReplaceLeftImage()
+        {
+            imageFiles.Remove(currentLeft);          // Delete the photo on the left
+            if (imageFiles.Count == 0) LoadImages();
+
+            string newImg;
+            do newImg = imageFiles[rnd.Next(imageFiles.Count)];
+            while (newImg == currentRight);          // Preventing duplication on the right
+
+            currentLeft = newImg;
+            pictureBoxLeft.ImageLocation = newImg;
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine().Trim(); // Reading data from Arduino
+
+            // Triggering changes in the GUI from the UI thread
+            this.Invoke(new Action(() =>
+            {
+                if (data == "A0")
+                {
+                    ReplaceRightImage();  // If a button is pressed on the Arduino, the left one wins
+                }
+                else if (data == "A1")
+                {
+                    ReplaceLeftImage();   // If a button is pressed on the Arduino, the right one wins
+                }
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e) => SendData("1"); // Send "1" to Arduino
+        private void button2_Click(object sender, EventArgs e) => SendData("2"); // Send "2" to Arduino
+        private void button3_Click(object sender, EventArgs e) => SendData("3"); // Send "3" to Arduino
+
+        private void SendData(string data)
+        {
+            if (serialPort.IsOpen)
+                serialPort.WriteLine(data); // Send data to Arduino
+            else
+                MessageBox.Show("No connection to the port!");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen)
+                serialPort.Close(); // Close the port when exiting the application
+        }
+
+        private void buttonRefreshPorts_Click(object sender, EventArgs e)
+        {
+            RefreshPorts();               // Refresh the list of ports
+            labelStatus.Text = "COM ports refreshed";
+        }
+    }
+}
